67281f9499cbf1953b6a259aaa9d73a6c5980f0d
giankd <giankd@gmail.com>
2022-08-13 11:15:08 +0200

    refactor(lsp): added some clarification line to messages

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -120,6 +120,8 @@ lsp_installer.setup({
 })
 
 local on_attach = function(client, bufnr)
+    print("Attaching " .. client.name)
+
 	if client.resolved_capabilities.document_formatting then
 		require("giankd.formatter").EnableFormatOnSave()
 	end
