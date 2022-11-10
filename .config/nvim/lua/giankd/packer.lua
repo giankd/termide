e053072834cb376962a119d896f9b10f67ed865f
ggiaccio <g.giaccio@ideasolutions.it>
2022-11-10 09:38:47 +0100

    fix(packer): removed flutter related stuff

--- .config/nvim/lua/giankd/packer.lua
+++ .config/nvim/lua/giankd/packer.lua
@@ -45,12 +45,6 @@ return require("packer").startup(function(use)
 	use("saadparwaiz1/cmp_luasnip")
 	use("simrat39/symbols-outline.nvim")
 
-	-- Dart / Flutter
-	use("dart-lang/dart-vim-plugin")
-	use("vim-flutter")
-	use("vim-lsc")
-	use("vim-lsc-dart")
-
 	-- Treesitter
 	use("nvim-treesitter/nvim-treesitter", {
 		run = ":TSUpdate",
