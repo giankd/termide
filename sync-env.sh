02d0411b50282b8a4f11b6f0ecf66e7d433a9757
ggiaccio <g.giaccio@ideasolutions.it>
2023-09-02 12:45:09 +0200

    feat(stow): better sync

--- sync-env.sh
+++ sync-env.sh
@@ -1,6 +1,17 @@
 #! /bin/sh
+set -e
+
 ROOT_USER_FOLDER=~/
 
-## Symlink config files
-stow -R --target=$ROOT_USER_FOLDER .
+# test for stow
+if ! command -v stow &> /dev/null
+then
+    echo "This script needs stow to be executed!"
+    exit
+fi
 
+echo "linking home directory"
+## Symlink config files
+stow -v -R --no-folding --target=$ROOT_USER_FOLDER .
+
+echo "Sync complete."
